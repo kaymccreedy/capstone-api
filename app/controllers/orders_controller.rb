53068225bc30b34afdbf3dd046431dfb45b13523
Kay McCreedy <thiskieranmccreedy@gmail.com>
2022-08-12 15:19:27 -0400

    updates methods in orders controller to have correct index and happy/sad paths

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,13 @@
 class OrdersController < ApplicationController
 
   def index
-    orders = Order.all.order(:id)
-    if orders.length > 0
+    orders = Order.where(user_id: current_user)
+    if orders.length > 0 && current_user
       render json: orders.as_json
-    else
+    elsif current_user
       render json: { error: "You have no orders!" }
+    else
+      render json: { error: "You must be logged in to view orders" }, status: :unauthorized
     end
   end
 
@@ -37,6 +39,7 @@ class OrdersController < ApplicationController
       render json: order.as_json
     else
       render json: { error: "You must be logged in to place an order" }, status: :unauthorized
+    end
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,4 @@ class Product < ApplicationRecord
   has_many :orders
   has_many :images
   
-
 end
